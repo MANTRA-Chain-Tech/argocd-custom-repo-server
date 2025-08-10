b6788d6920bb1a7a44c0a4e4dc8d9a4104cd81ce
Ryan Truong <ryan.truong@mantra.finance>
2025-08-10 16:50:47 +0700

    fix: render script containers debug logs in argocd appset yaml

--- render-kustomize.sh
+++ render-kustomize.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
-# Exit immediately if a command fails.
 set -e
 
-echo "--- Running Kustomize Renderer Plugin ---"
+# Redirect all echo statements to stderr, so they appear in logs but not in the final output.
+echo "--- Running Kustomize Renderer Plugin ---" >&2
+echo "Received Cluster Name: $ARGOCD_ENV_CLUSTER_NAME" >&2
 
 KUSTOMIZATION_FILE=$(find . -maxdepth 1 -iname 'kustomization.y*ml' | head -n 1)
 
 if [ -z "$KUSTOMIZATION_FILE" ]; then
-  echo "Error: No kustomization.yaml or kustomization.yml found."
+  echo "Error: No kustomization.yaml or kustomization.yml found." >&2
   exit 1
 fi
 
-echo "Found kustomization file: $KUSTOMIZATION_FILE"
+echo "Found kustomization file: $KUSTOMIZATION_FILE" >&2
 
 cat "$KUSTOMIZATION_FILE" | envsubst > kustomization.tmp.yaml
 
 mv kustomization.tmp.yaml "$KUSTOMIZATION_FILE"
 
-echo "--- Building manifests from processed kustomization file ---"
+echo "--- Building manifests from processed kustomization file ---" >&2
+# The final output of this command is the only thing sent to stdout
 kustomize build . --enable-helm
 
-echo "--- Kustomize Renderer Plugin Finished ---"
+echo "--- Kustomize Renderer Plugin Finished ---" >&2
