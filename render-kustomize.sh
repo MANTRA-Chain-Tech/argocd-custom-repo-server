aa04d113ab5a869ce2c3183d67e61e048b8f37be
Ryan Truong <ryan.truong@mantra.finance>
2025-08-11 16:10:02 +0700

    feat: non-compatible in render command

--- render-kustomize.sh
+++ render-kustomize.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 set -e
 
-# Redirect all echo statements to stderr for logging purposes
+# Redirect all echo statements to stderr for logging purposes.
 echo "--- Running Kustomize Renderer Plugin ---" >&2
-echo "Received Cluster Name: $ARGOCD_ENV_CLUSTER_NAME" >&2
 
-# Find all .yaml and .yml files in the current directory and all subdirectories
+VARS_TO_SUBSTITUTE=$(env | grep '^ARGOCD_ENV_' | sed -e 's/=\(.*\)//' -e 's/^/\$/g' | tr '\n' ' ')
+
+if [ -n "$VARS_TO_SUBSTITUTE" ]; then
+    echo "Found variables to substitute: ${VARS_TO_SUBSTITUTE}" >&2
+else
+    echo "No ARGOCD_ENV_ variables found to substitute." >&2
+fi
+
 echo "--- Finding and processing all YAML files for environment substitution ---" >&2
-find . -type f \( -name "*.yaml" -o -name "*.yml" \) -print0 | while IFS= read -r -d '' file; do
+find . -type f \( -name "*.yaml" -o -name "*.yml" \) | while IFS= read -r file; do
   echo "Processing file: $file" >&2
   tmp_file=$(mktemp)
-  cat "$file" | envsubst > "$tmp_file"
+  envsubst "${VARS_TO_SUBSTITUTE}" < "$file" > "$tmp_file"
   mv "$tmp_file" "$file"
 done
 
